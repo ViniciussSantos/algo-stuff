54ce4abbbd89c7a1be8331e5a06fb178dd8fc600
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-01-26 14:38:23 -0300

    making it faster

--- typescript/src/productExceptSelf.spec.ts
+++ typescript/src/productExceptSelf.spec.ts
@@ -1,11 +1,14 @@
 import { describe, it, expect } from "vitest";
 import { productExceptSelf } from "./productExceptSelf";
 
-describe("maxSubArray", () => {
+describe("productExceptSelf", () => {
   it("case 1", () => {
     expect(productExceptSelf([1, 2, 3, 4])).toStrictEqual([24, 12, 8, 6]);
   });
   it("case 2", () => {
     expect(productExceptSelf([-1, 1, 0, -3, 3])).toStrictEqual([0, 0, 9, 0, 0]);
+  });
+  it("case 3", () => {
+    expect(productExceptSelf([1, -1])).toStrictEqual([-1, 1]);
   });
 });
--- typescript/src/productExceptSelf.ts
+++ typescript/src/productExceptSelf.ts
@@ -1,17 +1,24 @@
-//TODO: complexity 0(n^2), find out a way of solving in a faster way and without using division
+//I use the prefix and postfix technique here
 export function productExceptSelf(nums: number[]): number[] {
-  let res: number[] = [];
+  const res: number[] = [1];
 
-  for (let i = 0; i < nums.length; i++) {
-    res.push(
-      Math.abs(
-        nums.reduce(
-          (acc, curr, index) => (index === i ? acc * 1 : acc * curr),
-          1,
-        ),
-      ),
-    );
+  //in this case, the prefix is always the last element of the array
+  //at a given iteration
+  for (let i = 0; i < nums.length - 1; i++) {
+    res.push(res[res.length - 1] * nums[i]);
   }
 
-  return res;
+  //When walking backwards, I have to use the ptr variable to offset the position of nums array
+  //from the postfix
+  let postfix = 1;
+  let ptr = nums.length - 2;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    postfix *= nums[i];
+    res[ptr] *= postfix;
+    ptr--;
+  }
+
+  // I do this because the leetcode tester using loosely equal (I think) while I use strictly equal in my tests
+  return res.map((v) => (v === -0 ? Math.abs(v) : v));
 }
